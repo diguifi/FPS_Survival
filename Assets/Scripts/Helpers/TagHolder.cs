9e11177444173d8ae1b964332dcfa769cd60b9e3
diguifi <lumia-diguifi@hotmail.com>
2019-08-29 23:37:54 -0300

    Add basic movement and jump

---
+++ Assets/Scripts/Helpers/TagHolder.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Axis
+{
+    public const string HORIZONTAL = "Horizontal";
+    public const string VERTICAL = "Vertical";
+}
---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private CharacterController characterController;
+    private Vector3 moveDirection;
+    private float verticalVelocity;
+    private float gravity = 20f;
+    public float speed = 5f;
+    public float jumpForce = 10f;
+
+    void Awake()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        moveDirection = new Vector3(Input.GetAxis(Axis.HORIZONTAL), 0f, Input.GetAxis(Axis.VERTICAL));
+        moveDirection = transform.TransformDirection(moveDirection);
+        moveDirection *= speed * Time.deltaTime;
+
+        ApplyGravity();
+
+        characterController.Move(moveDirection);
+    }
+
+    void ApplyGravity()
+    {
+        verticalVelocity -= gravity * Time.deltaTime;
+        Jump();
+        moveDirection.y = verticalVelocity * Time.deltaTime;
+    }
+
+    void Jump()
+    {
+        if (characterController.isGrounded && Input.GetKeyDown(KeyCode.Space))
+            verticalVelocity = jumpForce;
+    }
+}
