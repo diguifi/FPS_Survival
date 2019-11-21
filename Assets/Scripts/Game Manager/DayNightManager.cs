b3be615c757d7a1887472ce46400f661de09313a
diguifi <lumia-diguifi@hotmail.com>
2019-11-21 12:18:20 -0300

    Create basic day/night cycles

---
+++ Assets/Scripts/Game Manager/DayNightManager.cs
@@ -0,0 +1,157 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DayNightManager : MonoBehaviour
+{
+    [Header("Time")]
+    [Tooltip("Day Length in Minutes")]
+    [SerializeField]
+    private float _targetDayLength = 10f; //length of day in minutes
+    public float targetDayLength
+    {
+        get
+        {
+            return _targetDayLength;
+        }
+    }
+    [SerializeField]
+    private float elapsedTime;
+    [SerializeField]
+    private bool use24Clock = true;
+    [SerializeField]
+    private Text clockText;
+    [SerializeField]
+    [Range(0f, 1f)]
+    private float _timeOfDay;
+    public float timeOfDay
+    {
+        get
+        {
+            return _timeOfDay;
+        }
+    }
+    [SerializeField]
+    private int _dayNumber = 0; //tracks the days passed
+    public int dayNumber
+    {
+        get
+        {
+            return _dayNumber;
+        }
+    }
+    [SerializeField]
+    private int _yearNumber = 0;
+    public int yearNumber
+    {
+        get
+        {
+            return _yearNumber;
+        }
+    }
+    private float _timeScale = 100f;
+    [SerializeField]
+    private int _yearLength = 100;
+    public float yearLength
+    {
+        get
+        {
+            return _yearLength;
+        }
+    }
+    public bool pause = false;
+    [SerializeField]
+    private AnimationCurve timeCurve;
+    private float timeCurveNormalization;
+
+    [Header("Sun Light")]
+    [SerializeField]
+    private Transform dailyRotation;
+    [SerializeField]
+    private Light sun;
+    private float intensity;
+    [SerializeField]
+    private float sunBaseIntensity = 1f;
+    [SerializeField]
+    private float sunVariation = 1.5f;
+    [SerializeField]
+    private Gradient sunColor;
+
+    private void Start()
+    {
+        NormalTimeCurve(); 
+    }
+
+    private void Update()
+    {
+        if (!pause)
+        {
+            UpdateTimeScale();
+            UpdateTime();
+        }
+
+        AdjustSunRotation();
+        SunIntensity();
+        AdjustSunColor();
+    }
+
+    private void UpdateTimeScale()
+    {
+        _timeScale = 24 / (_targetDayLength / 60);
+        _timeScale *= timeCurve.Evaluate(elapsedTime / (targetDayLength * 60)); //changes timescale based on time curve
+        _timeScale /= timeCurveNormalization; //keeps day length at target value
+    }
+
+    private void NormalTimeCurve()
+    {
+        float stepSize = 0.01f;
+        int numberSteps = Mathf.FloorToInt(1f / stepSize);
+        float curveTotal = 0;
+
+        for (int i = 0; i < numberSteps; i++)
+        {
+            curveTotal += timeCurve.Evaluate(i * stepSize);
+        }
+
+        timeCurveNormalization = curveTotal / numberSteps; //keeps day length at target value
+    }
+
+    private void UpdateTime()
+    {
+        _timeOfDay += Time.deltaTime * _timeScale / 86400; // seconds in a day
+        elapsedTime += Time.deltaTime;
+        if(_timeOfDay > 1) //new day!!
+        {
+            elapsedTime = 0;
+            _dayNumber++;
+            _timeOfDay -= 1;
+
+            if(_dayNumber > _yearLength) //new year!
+            {
+                _yearNumber++;
+                _dayNumber = 0;
+            }
+        }
+    }
+
+    //rotates the sun daily
+    private void AdjustSunRotation()
+    {
+        float sunAngle = timeOfDay * 360f;
+        dailyRotation.transform.localRotation = Quaternion.Euler(new Vector3(0f, 0f, sunAngle));
+    }
+
+    private void SunIntensity()
+    {
+        intensity = Vector3.Dot(sun.transform.forward, Vector3.down);
+        intensity = Mathf.Clamp01(intensity);
+
+        sun.intensity = intensity * sunVariation + sunBaseIntensity;
+    }
+
+    private void AdjustSunColor()
+    {
+        sun.color = sunColor.Evaluate(intensity);
+    }
+}
