4f8ca016cc3f78435af266126e0da7708a14a277
diguifi <lumia-diguifi@hotmail.com>
2019-11-22 23:30:36 -0300

    Creates simple Main Menu
    Only play and exit options

---
+++ Assets/Scripts/Menu/MainMenuManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+
+    public void QuitGame()
+    {
+        UnityEditor.EditorApplication.isPlaying = false;
+        Application.Quit();
+    }
+}
