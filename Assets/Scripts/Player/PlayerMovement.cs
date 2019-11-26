7b165f247d9618d345f5795c68d43239c4dbd7de
diguifi <lumia-diguifi@hotmail.com>
2019-11-25 21:53:40 -0300

    Implements fall damage

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -5,21 +5,25 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     private CharacterController characterController;
+    private Health healthManager;
     private Vector3 moveDirection;
     private float verticalVelocity;
     private float gravity = 20f;
+    private float timeFalling = 0f;
     public float speed = 4f;
     public float jumpForce = 10f;
 
     void Awake()
     {
         characterController = GetComponent<CharacterController>();
+        healthManager = GetComponent<Health>();
     }
 
     // Update is called once per frame
     void Update()
     {
         Move();
+        CheckFallDamage();
     }
 
     void Move()
@@ -43,6 +47,22 @@ public class PlayerMovement : MonoBehaviour
     void Jump()
     {
         if (characterController.isGrounded && Input.GetKeyDown(KeyCode.Space))
+        {
             verticalVelocity = jumpForce;
+        }
+    }
+
+    void CheckFallDamage()
+    {
+        if (!characterController.isGrounded)
+        {
+            timeFalling += Time.deltaTime;
+        }
+        else
+        {
+            if (timeFalling > 1.3f)
+                healthManager.DealDamage((timeFalling * timeFalling) * 2);
+            timeFalling = 0f;
+        }
     }
 }
